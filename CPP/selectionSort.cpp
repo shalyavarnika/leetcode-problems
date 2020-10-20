6a5368bfb907aeb62f383b5fcba13ee5f665b89b
goyalshubhangi <goyalshubhangi72@gmail.com>
2020-10-20 09:51:46 +0530

    Sorting algorithms

---
+++ CPP/selectionSort.cpp
@@ -0,0 +1,35 @@
+/*
+Selection sort algorithm
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int size, arr[50], i, j, temp;
+	cout<<"Enter Array Size : ";
+	cin>>size;
+	cout<<"Enter Array Elements : ";
+	for(i=0; i<size; i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Sorting array using selection sort...\n";
+	for(i=0; i<size; i++)
+	{
+		for(j=i+1; j<size; j++)
+		{
+			if(arr[i]>arr[j])
+			{
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	cout<<"Now the Array after sorting is :\n";
+	for(i=0; i<size; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<"\nThe total number of comparisions required are:"<<(size-1);
+}
